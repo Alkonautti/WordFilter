d46167de31e1ae6cdc668e93764a53fe9e18ad7f
Niko <niko.kilponen@gmail.com>
2022-09-21 11:31:43 +0300

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,199 @@
+﻿
+namespace WordFilter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.btn_add = new System.Windows.Forms.Button();
+            this.btn_remove = new System.Windows.Forms.Button();
+            this.btn_save = new System.Windows.Forms.Button();
+            this.btn_exit = new System.Windows.Forms.Button();
+            this.txtbox_addword = new System.Windows.Forms.TextBox();
+            this.btn_filter = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(92, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Text to be filtered:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(13, 30);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(355, 526);
+            this.textBox1.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(732, 14);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(63, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Filter words:";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(735, 30);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(164, 472);
+            this.listBox1.TabIndex = 3;
+            // 
+            // btn_add
+            // 
+            this.btn_add.Location = new System.Drawing.Point(735, 534);
+            this.btn_add.Name = "btn_add";
+            this.btn_add.Size = new System.Drawing.Size(75, 23);
+            this.btn_add.TabIndex = 4;
+            this.btn_add.Text = "Add";
+            this.btn_add.UseVisualStyleBackColor = true;
+            this.btn_add.Click += new System.EventHandler(this.btn_add_Click);
+            // 
+            // btn_remove
+            // 
+            this.btn_remove.Location = new System.Drawing.Point(824, 534);
+            this.btn_remove.Name = "btn_remove";
+            this.btn_remove.Size = new System.Drawing.Size(75, 23);
+            this.btn_remove.TabIndex = 5;
+            this.btn_remove.Text = "Remove";
+            this.btn_remove.UseVisualStyleBackColor = true;
+            this.btn_remove.Click += new System.EventHandler(this.btn_remove_Click);
+            // 
+            // btn_save
+            // 
+            this.btn_save.Location = new System.Drawing.Point(735, 564);
+            this.btn_save.Name = "btn_save";
+            this.btn_save.Size = new System.Drawing.Size(75, 23);
+            this.btn_save.TabIndex = 6;
+            this.btn_save.Text = "Save";
+            this.btn_save.UseVisualStyleBackColor = true;
+            this.btn_save.Click += new System.EventHandler(this.btn_save_Click);
+            // 
+            // btn_exit
+            // 
+            this.btn_exit.Location = new System.Drawing.Point(824, 563);
+            this.btn_exit.Name = "btn_exit";
+            this.btn_exit.Size = new System.Drawing.Size(75, 23);
+            this.btn_exit.TabIndex = 7;
+            this.btn_exit.Text = "Exit";
+            this.btn_exit.UseVisualStyleBackColor = true;
+            this.btn_exit.Click += new System.EventHandler(this.btn_exit_Click);
+            // 
+            // txtbox_addword
+            // 
+            this.txtbox_addword.Location = new System.Drawing.Point(735, 508);
+            this.txtbox_addword.Name = "txtbox_addword";
+            this.txtbox_addword.Size = new System.Drawing.Size(164, 20);
+            this.txtbox_addword.TabIndex = 8;
+            // 
+            // btn_filter
+            // 
+            this.btn_filter.Location = new System.Drawing.Point(293, 563);
+            this.btn_filter.MaximumSize = new System.Drawing.Size(75, 23);
+            this.btn_filter.MinimumSize = new System.Drawing.Size(75, 23);
+            this.btn_filter.Name = "btn_filter";
+            this.btn_filter.Size = new System.Drawing.Size(75, 23);
+            this.btn_filter.TabIndex = 9;
+            this.btn_filter.Text = "Filter text";
+            this.btn_filter.UseVisualStyleBackColor = true;
+            this.btn_filter.Click += new System.EventHandler(this.btn_filter_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(374, 30);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(355, 526);
+            this.textBox2.TabIndex = 10;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(371, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(64, 13);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Filtered text:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(913, 602);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.btn_filter);
+            this.Controls.Add(this.txtbox_addword);
+            this.Controls.Add(this.btn_exit);
+            this.Controls.Add(this.btn_save);
+            this.Controls.Add(this.btn_remove);
+            this.Controls.Add(this.btn_add);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(929, 641);
+            this.MinimumSize = new System.Drawing.Size(929, 641);
+            this.Name = "Form1";
+            this.Text = "Word Filter";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button btn_add;
+        private System.Windows.Forms.Button btn_remove;
+        private System.Windows.Forms.Button btn_save;
+        private System.Windows.Forms.Button btn_exit;
+        private System.Windows.Forms.TextBox txtbox_addword;
+        private System.Windows.Forms.Button btn_filter;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+using System.IO;
+
+namespace WordFilter
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            LoadFilterWords(); //Load saved filter words
+            textBox1.Multiline = true;
+            textBox1.ScrollBars = ScrollBars.Vertical;
+        }
+
+        private void LoadFilterWords()
+        {
+            string path = Application.StartupPath + "\\words.txt";  //path to save file
+
+            if (File.Exists(path)) //if file exists
+            {
+                StreamReader reader = new StreamReader(path, Encoding.Default); //make new streamwriter instance
+
+                string word;
+                while ((word = reader.ReadLine()) != null) //while save file has not empty line, read it and add it to listbox
+                {
+                    listBox1.Items.Add(word);
+                }
+
+                reader.Close();
+                RemoveEmptyInFilters(); //re´move empty items in listbox
+            }
+        }
+
+        private void RemoveEmptyInFilters()
+        {
+            int count = listBox1.Items.Count; //count of items in listbox
+            for (int i = count - 1; i >= 0; i--) //iterate thru all items in listbox and remove empty ones
+            {
+                if (listBox1.Items[i].ToString() == "")
+                {
+                    listBox1.Items.RemoveAt(i);
+                }
+            }
+        }
+
+        private void btn_add_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Add(txtbox_addword.Text); //add filter word to listbox
+            txtbox_addword.Clear(); //clear text field
+            RemoveEmptyInFilters(); //remove empty items in listbox
+        }
+
+        private void btn_exit_Click(object sender, EventArgs e)
+        {
+            Application.Exit(); //exit app
+        }
+
+        private void btn_remove_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.RemoveAt(listBox1.SelectedIndex); //remove item from listbox
+        }
+
+        private void btn_filter_Click(object sender, EventArgs e)
+        {
+            string[] splittedText = textBox1.Text.Split(' '); //split textbox text into single words
+
+            foreach (string filterWord in listBox1.Items) //iterate thru words and check if it matches to a filter word
+            {
+                Regex reg = new Regex(filterWord.ToString());
+
+                for (int i = 0; i < splittedText.Length; i++)
+                {
+                    if (reg.IsMatch(splittedText[i]))
+                    {
+                        splittedText[i] = new string('*', splittedText[i].Length); //if match replace word with ****
+                    }
+                }
+
+            }
+
+            textBox2.Clear(); //clear textbox
+
+            foreach (string str in splittedText) //add filtered text to textbox
+            {
+                textBox2.Text += str;
+                textBox2.Text += " ";
+            }
+        }
+
+        private void btn_save_Click(object sender, EventArgs e)
+        {
+            string[] filterWords = listBox1.Items.OfType<string>().ToArray(); //make an array from filter words
+
+            string path = Application.StartupPath + "\\words.txt"; //path to save file
+
+            if (filterWords.Length > 0) //check if there is actually a word to save
+            {
+                File.WriteAllLines(path, filterWords); //write word to save file
+            }
+
+            else
+            {
+                MessageBox.Show("No filter words in list, data not saved."); //message box if there is no filter words to be saved
+            }
+        }
+    }
+}
